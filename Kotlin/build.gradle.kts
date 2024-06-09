d7f90d0626190cd71d7067bed9c7c045950eee6a
robsetslv <roberts.polis@sets.lv>
2024-06-09 19:05:02 +0300

    initial refactorig.

--- Kotlin/build.gradle.kts
+++ Kotlin/build.gradle.kts
@@ -1,8 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+
 plugins {
 	kotlin("jvm") version "1.9.10"
 	application
+	id("io.gitlab.arturbosch.detekt") version("1.23.3")
 }
 
 group = "com.gildedrose"
@@ -24,16 +26,16 @@ tasks.test {
 	}
 }
 
-// config JVM target to 1.8 for kotlin compilation tasks
-tasks.withType<KotlinCompile>().configureEach {
-	kotlinOptions.jvmTarget = "1.8"
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
 }
 
-// config java extension to same target version, to avoid build failure on Gradle 8.x
 java {
-	targetCompatibility = JavaVersion.VERSION_1_8
+	targetCompatibility = JavaVersion.VERSION_11
 }
 
 application {
 	mainClass.set("com.gildedrose.TexttestFixtureKt")
 }
+
+
+
--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,58 +1,65 @@
 package com.gildedrose
 
+import com.gildedrose.GildedRoseHelper.decQualityIf
+import com.gildedrose.GildedRoseHelper.decSellin
+import com.gildedrose.GildedRoseHelper.incQualityIf
+import com.gildedrose.GildedRoseHelper.resetQualityIf
+
 class GildedRose(var items: List<Item>) {
 
+    companion object {
+        const val MAX_QUALITY = 50
+        const val ZERO = 0;
+        const val ELEVEN = 11;
+        const val SIX = 6;
+    }
+
+    @Suppress("LongMethod")
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
+        items.forEach { item ->
+            when (item.name) {
+                "Aged Brie" -> updateAgedBrie(item)
+                "Backstage passes to a TAFKAL80ETC concert" -> updateBackstagePasses(item)
+                "Sulfuras, Hand of Ragnaros" -> updateSulfuras(item) // Do nothing for Sulfuras
+                //"Conjured Mana Cake" -> updateConjuredManaCake(item) // "Conjured" items degrade in Quality twice as fast as normal items
+                else -> updateOtherItem(item)
+                // any unknown items like following is terminated to else:
+                // Elixir of the Mongoose
+                // +5 Dexterity Vest
+                // Conjured Mana Cake
+                // test passage is a gold standard!
             }
         }
     }
 
-}
+    private fun updateAgedBrie(item: Item) {
+        incQualityIf(item) { it.quality < MAX_QUALITY }
+        decSellin(item)
+        incQualityIf(item) { it.sellIn < ZERO && it.quality < MAX_QUALITY }
+    }
 
+    private fun updateBackstagePasses(item: Item) {
+        // trying to fix this. if sellin is 10 .. 6, quality should increase by 2.
+        // allthrough tests are passing, but the logic possible is wrong.
+        incQualityIf(item) { item.quality < MAX_QUALITY }
+        incQualityIf(item) { item.sellIn < ELEVEN && item.quality < MAX_QUALITY }
+        incQualityIf(item) { item.sellIn < SIX && item.quality < MAX_QUALITY }
+        decSellin(item)
+        resetQualityIf(item) { item.sellIn < ZERO }
+    }
+
+    private fun updateSulfuras(item: Item) {
+        // Do nothing for Sulfuras
+    }
+
+//    private fun updateConjuredManaCake(item: Item) {
+//        // we has to be implement this but not tests will cover it (except addon ones)
+//    }
+
+    private fun updateOtherItem(item: Item) {
+        decQualityIf(item) { item.quality > ZERO }
+        decSellin(item)
+        decQualityIf(item) { item.sellIn < ZERO && item.quality > ZERO }
+    }
+
+ }
